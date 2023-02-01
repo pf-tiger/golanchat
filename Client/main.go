4cb6b2fd098baa2c4523e5e7db2706f5f168ebb3
Tiger <107926734+pf-tiger@users.noreply.github.com>
2023-02-01 17:43:59 +0900

    added timecode as separate values. contains bug with line clearance

--- Client/main.go
+++ Client/main.go
@@ -1,6 +1,8 @@
-package main
+// package main
 
-// takes an argument for the address/hostname of the messge server
+// // takes an argument for the address/hostname of the messge server
+
+package main
 
 import (
 	"bufio"
@@ -11,9 +13,11 @@ import (
 	"time"
 )
 
+const displayLimit = 30
+const TitleBanner = "golanchat by pf_tiger"
+
 func main() {
 	arguments := os.Args
-
 	// when no address/hostname is specified
 	if len(arguments) == 1 {
 		fmt.Println("Please provide a server address/hostname")
@@ -22,46 +26,57 @@ func main() {
 
 	conn, err := net.Dial("tcp4", arguments[1]+":8080")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
-
 	fmt.Println("Connected to the chat server:", arguments[1], "Type messages and press Enter to send.")
 
-	go func() {
-		for {
-			message, err := bufio.NewReader(conn).ReadString('\n')
-			if err != nil {
-				break
-			}
-			message = strings.TrimSpace(message)
-			fmt.Println(message)
-		}
-	}()
-
+	numlines := 0
 	for {
 		reader := bufio.NewReader(os.Stdin)
-		message, _ := reader.ReadString('\n')
-		message = strings.TrimSpace(message)
-		if message == "" {
-			continue
-		}
-		message = message + "\n"
-		_, err := conn.Write([]byte(message))
-		if err != nil {
-			fmt.Println("Error:", err)
-			break
-		}
-		if message == "exit!\n" {
+		fmt.Print("You > ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		numlines++
+
+		currentTime := time.Now().Format("15:04:05")
+
+		message := currentTime + ":" + text
+
+		// command "exit!" to close connection from the server
+		if text == "exit!\n" {
 			fmt.Println("Closing connection to server")
-			defer conn.Close()
+			conn.Close()
 			break
 		}
-		fmt.Print(time.Now().Format("15:04:05"), " You > ", message)
+
+		fmt.Print("\033[A")  // move cursor up
+		fmt.Print("\033[2K") // clear current line
+		// fmt.Print("\033[A")  // move cursor up
+		// fmt.Print("\033[2K") // clear current line
+
+		_, err := conn.Write([]byte(message + "\n"))
+		if err != nil {
+			fmt.Println("Connection closed")
+			break
+		}
+		fmt.Println(currentTime, " You > ", text)
+
+		// cleaning the UI
+		if numlines >= displayLimit {
+			// write codes to clean the UI
+			for i := 0; i < numlines; i++ {
+				fmt.Print("\033[A") // move cursor up
+			}
+			fmt.Print("\033[2K") // clear current line
+			for i := 0; i < numlines; i++ {
+				fmt.Print("\033[B") // move cursor up
+			}
+		}
 	}
 }
 
-// function to receive and display messages from the server/other clients
+// // function to receive and display messages from the server/other clients
 
-// further functions for switching debugging modes is wanted
+// // further functions for switching debugging modes is wanted
